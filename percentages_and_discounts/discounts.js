c94048663d3dc8a4ca01aa9b687cbbc5524ec03f
santiagobr <santiagobastidasrodriguez@gmail.com>
2021-08-05 12:58:48 -0500

    statistics project start

--- percentages_and_discounts/discounts.js
+++ percentages_and_discounts/discounts.js
@@ -1,41 +1,49 @@
-function calcPriceWithDiscount(price, coupon) {
-    const percentageCode = discountCoupon(coupon)
-    const percentageWithDiscount = 100 - percentageCode
-    const priceWithDiscount = (price * percentageWithDiscount) / 100
+const coupons = [
+    {
+        name: 'promocion-15',
+        discount: 15
+    },
+    {
+        name: 'promocion-30',
+        discount: 30
+    },
+    {
+        name: 'promocion-50',
+        discount: 50
+    }
+]
+
+function calcPriceWithDiscount (price, coupon) {
+    const percentageWithDiscount = 100 -coupon
+    const priceWithDiscount = (price * percentageWithDiscount) /100
 
     return priceWithDiscount
 }
 
-function discountCoupon (code) {
-    let discount 
-
-    switch (code) {
-        case 'promocion-15':
-            return discount = 15
-            break;
-        case 'promocion-30':
-            return discount = 30
-            break;
-        case 'promocion-50':
-            return discount = 50
-            break;
-    
-        default: 
-            return discount = 0
-            break;
-    }
-}
-
-function priceDiscountBtn () 
-{
+function priceDiscountBtn() {
     const PriceInput = document.getElementById('PriceInput')
     const priceValue = PriceInput.value
-    
+
     const CouponInput = document.getElementById('CouponInput')
     const couponValue = CouponInput.value
 
-    const priceWithDiscount = calcPriceWithDiscount(priceValue, couponValue)
+    
+    const  isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue
+    }
+    
+    const userCoupon = coupons.find(isCouponValueValid)
+        
+    if(!userCoupon) {
+        const PriceOutput = document.getElementById('PriceOutput')
+        PriceOutput.innerText = `El cupón ${couponValue} no es válido. El precio del producto es: ${priceValue}`
+    } else {
+        const PriceOutput = document.getElementById('PriceOutput')
+        const discount = userCoupon.discount
 
-    const PriceOutput = document.getElementById('PriceOutput')
-    PriceOutput.innerText = `El precio con descuento es:  $${priceWithDiscount}`
+        const priceWithDiscount = calcPriceWithDiscount(priceValue, discount)
+
+        PriceOutput.innerText = `El precio con descuento es ${priceWithDiscount}`
+    }
+
 }
