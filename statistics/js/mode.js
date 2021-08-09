b973e9a5e839a3282d1eda574ebf4e329a393cce
santiagobr <santiagobastidasrodriguez@gmail.com>
2021-08-09 16:14:56 -0500

    mode project and class solution

--- statistics/js/mode.js
+++ statistics/js/mode.js
@@ -1,17 +1,24 @@
 const list = [1, 2, 3, 1, 1, 2, 2, 3, 2, 2, 4, 5, 500];
 
-const listCount = {}
+function calcMode (list) {
 
-list.map(
-	function (element) {
-		if(listCount[element]){
-			listCount[element] += 1
-		} else {
-			listCount[element] = 1
+	const listCount = {}
+	
+	list.map(
+		function (element) {
+			if(listCount[element]){
+				listCount[element] += 1
+			} else {
+				listCount[element] = 1
+			}
 		}
-	}
-)
+	)
+	
+	const listNewArray = Object.entries(listCount).sort((a,b) => a[1] - b[1])
+	
+	const mode = listNewArray[listNewArray.length - 1]
 
-const listNewArray = Object.entries(listCount).sort((a,b) => a[1] - b[1])
+	return mode
+}
 
-const mode = listNewArray[listNewArray.length - 1]
+
